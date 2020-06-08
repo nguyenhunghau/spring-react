4788e567700c0bba686b27178d0fe3d8f50d21ee
Hau <nguyenhunghau@fabercompany.co.jp>
2020-06-08 17:20:22 +0700

    Update deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 # Create folder includes code
-mkdir -p /deploy/server&& cp docker-compose.yml /deploy/ && cd server && mvn clean install
+mkdir -p deploy/server &&
+cp docker-compose.yml deploy/ &&
+cd server &&
+mvn clean install;
+echo "Finish create jar file"
+mv target/*.jar target/app.jar &&
+cd .. &&
+mkdir deploy/server/target &&
+cp -v server/target/app.jar deploy/server/target/;
+echo "Finish copy jar file to deploy folder"
+cp server/Dockerfile deploy/server/ &&
+mkdir -p deploy/client && rsync -avr --exclude='node_modules' client/ deploy/client/
