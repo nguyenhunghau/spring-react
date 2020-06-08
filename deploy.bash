739b8c6230c77b37eac6ddc5c896e6fcd1d682f9
Hau <nguyenhunghau@fabercompany.co.jp>
2020-06-08 17:41:44 +0700

    Update deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -11,4 +11,7 @@ mkdir deploy/server/target &&
 cp -v server/target/app.jar deploy/server/target/;
 echo "Finish copy jar file to deploy folder"
 cp server/Dockerfile deploy/server/ &&
-mkdir -p deploy/client && rsync -avr --exclude='node_modules' client/ deploy/client/
+mkdir -p deploy/client && rsync -avr --exclude='node_modules' client/ deploy/client/;
+echo "Finish make data local"
+rsync rsync -avzhe "ssh -i /mnt/ec2-sin.pem" deploy/ ec2-user@54.255.250.93:/home/ec2-user/;
+echo "Finish upload file to server"
