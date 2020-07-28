dde48f5ff884384ffeb9107c44d432a8b88cd907
Hau <nguyenhunghau@fabercompany.co.jp>
2020-07-28 17:08:50 +0700

    Add server cloud

--- server/src/main/java/com/example/management/ManagementApplication.java
+++ server/src/main/java/com/example/management/ManagementApplication.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@EnableEurekaServer
+@EnableEurekaServer
 @SpringBootApplication(
         exclude = {
             org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,}
