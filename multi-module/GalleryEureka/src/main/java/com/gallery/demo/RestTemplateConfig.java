eaa59c44f27450e40c51f75e683ae1a4c8e6d0c4
Hau <nguyenhunghau@fabercompany.co.jp>
2020-07-29 16:13:34 +0700

    make multi module for spring cloud

---
+++ multi-module/GalleryEureka/src/main/java/com/gallery/demo/RestTemplateConfig.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gallery.demo;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+class RestTemplateConfig {
+
+    // Create a bean for restTemplate to call services  
+    @Bean
+    @LoadBalanced // Load balance between service instances running at different ports.  
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
